b2d22772a2dc013adc33cbcd0b37c79a89343d6d
Jack Lund <jacklund@Jacks-MacBook-Pro.local>
2023-10-16 18:24:39 -0700

    Set up some of the api and html routes

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
+const htmlroutes = require('./routes/htmlroutes');
+const apiroutes = require('./routes/apiroutes')
 const express = require('express');
 const PORT = 3001;
-const path = require('path');
-
 const app = express();
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended : true}));
+app.use(express.static('public'));
+app.use('/api', apiroutes);
+app.use('/', htmlroutes);
+
+
+app.listen(PORT, () => {
+    console.log('listening on port 3001');
+});
