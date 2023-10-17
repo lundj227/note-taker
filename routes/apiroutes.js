a8384932f5da2f25680689fab62c03be91658c07
Jack Lund <jacklund@Jacks-MacBook-Pro.local>
2023-10-16 22:00:37 -0700

    Added functionality to save notes need to be able to access them again though

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,8 +1,18 @@
 const router = require('express').Router();
 const db = require('../db/db.json');
+const fs = require('fs');
+const path = require('path');
 
 router.get('/notes', (req, res) => {
     return res.json(db);
+});
+
+router.post('/notes', (req, res) => {
+    var notesDb = require('../db/db.json');
+    notesDb.push(req.body);
+    console.log(notesDb);
+    fs.writeFileSync(path.join(__dirname, '../db/db.json'), JSON.stringify(notesDb));
+    res.json(notesDb);
 });
 
 module.exports = router;
