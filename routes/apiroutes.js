876926f03d859711690e7764b33edca4be7271b3
Jack Lund <jacklund@Jacks-MacBook-Pro.local>
2023-10-17 18:17:31 -0700

    Added unique ids

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -2,6 +2,8 @@ const router = require('express').Router();
 const db = require('../db/db.json');
 const fs = require('fs');
 const path = require('path');
+const ShortUniqueId = require('short-unique-id');
+const uid = new ShortUniqueId({ length: 10 });
 
 router.get('/notes', (req, res) => {
     return res.json(db);
@@ -9,7 +11,11 @@ router.get('/notes', (req, res) => {
 
 router.post('/notes', (req, res) => {
     var notesDb = require('../db/db.json');
-    notesDb.push(req.body);
+    notesDb.push({
+        title: req.body.title,
+        text: req.body.text,
+        id: uid.rnd()
+    }) // (req.body); // uid.rnd()
     console.log(notesDb);
     fs.writeFileSync(path.join(__dirname, '../db/db.json'), JSON.stringify(notesDb));
     res.json(notesDb);
